7b1e80476c61a31d17b93ae246ce2bfbe1b3d5de
Luis Barroso <luis.barroso@corilus.be>
2025-07-26 22:07:10 +0200

    Eight stage

--- src/command.rs
+++ src/command.rs
@@ -4,7 +4,7 @@ use thiserror::Error;
 use tokio::time::Instant;
 
 use crate::{
-    key_value_store::{KeyValueStore, Value},
+    key_value_store::{DataType, KeyValueStore, Value},
     resp::RespValue,
 };
 
@@ -24,6 +24,10 @@ pub enum CommandError {
     InvalidSetCommandArgument,
     #[error("invalid SET command expiration")]
     InvalidSetCommandExpiration,
+    #[error("invalid RPUSH command")]
+    InvalidRPushCommand,
+    #[error("data not found")]
+    DataNotFound,
 }
 
 impl CommandError {
@@ -36,6 +40,8 @@ impl CommandError {
             CommandError::InvalidSetCommand => b"-ERR invalid SET command\r\n",
             CommandError::InvalidSetCommandArgument => b"-ERR invalid SET command argument\r\n",
             CommandError::InvalidSetCommandExpiration => b"-ERR invalid SET command expiration\r\n",
+            CommandError::InvalidRPushCommand => b"-ERR invalid RPUSH command\r\n",
+            CommandError::DataNotFound => b"-ERR data not found\r\n",
         }
     }
 }
@@ -82,7 +88,6 @@ pub fn handle_command(
     store: &mut KeyValueStore,
 ) -> Result<String, CommandError> {
     let command = Command::new(input)?;
-    println!("Received command: {:?}", command);
 
     match command.name.as_str() {
         "PING" => Ok("+PONG\r\n".to_string()),
@@ -102,15 +107,22 @@ pub fn handle_command(
             let stored_data = store.get(&command.args[0]);
 
             match stored_data {
-                Some(data) => {
-                    if let Some(expiration) = data.expiration {
+                Some(value) => {
+                    if let Some(expiration) = value.expiration {
                         if Instant::now() > expiration {
                             store.remove(&command.args[0]);
                             return Ok("$-1\r\n".to_string());
                         }
                     }
 
-                    return Ok(format!("${}\r\n{}\r\n", data.value.len(), data.value));
+                    match value.data {
+                        DataType::String(ref s) => {
+                            return Ok(format!("${}\r\n{}\r\n", s.len(), s));
+                        }
+                        _ => {
+                            return Ok("$-1\r\n".to_string());
+                        }
+                    }
                 }
                 None => {
                     return Ok("$-1\r\n".to_string());
@@ -139,12 +151,42 @@ pub fn handle_command(
             store.insert(
                 command.args[0].clone(),
                 Value {
-                    value: command.args[1].clone(),
+                    data: DataType::String(command.args[1].clone()),
                     expiration,
                 },
             );
 
             return Ok("+OK\r\n".to_string());
+        }
+        "RPUSH" => {
+            if command.args.len() < 2 {
+                return Err(CommandError::InvalidRPushCommand);
+            }
+
+            let mut array_length = 0;
+
+            store
+                .entry(command.args[0].clone())
+                .and_modify(|v| {
+                    if let DataType::Array(ref mut list) = v.data {
+                        list.push(command.args[1].clone());
+                        array_length = list.len();
+                    }
+                })
+                .or_insert_with(|| {
+                    array_length = 1;
+
+                    return Value {
+                        data: DataType::Array(vec![command.args[1].clone()]),
+                        expiration: None,
+                    };
+                });
+
+            if array_length == 0 {
+                return Err(CommandError::DataNotFound);
+            }
+
+            return Ok(format!(":{}\r\n", array_length));
         }
         _ => {
             return Err(CommandError::InvalidEchoCommand);
--- src/key_value_store.rs
+++ src/key_value_store.rs
@@ -1,9 +1,28 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, str::FromStr};
 
 use tokio::time::Instant;
 
+pub enum DataType {
+    String(String),
+    Array(Vec<String>),
+}
+
+impl FromStr for DataType {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<DataType, Self::Err> {
+        match input {
+            "Bar" => Ok(DataType::String("Bar".into())),
+            "Baz" => Ok(DataType::String("Baz".into())),
+            "Bat" => Ok(DataType::String("Bat".into())),
+            "Quux" => Ok(DataType::String("Quux".into())),
+            _ => Err(()),
+        }
+    }
+}
+
 pub struct Value {
-    pub value: String,
+    pub data: DataType,
     pub expiration: Option<Instant>,
 }
 
